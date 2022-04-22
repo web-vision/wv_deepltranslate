d93abff08ffef2670c3814c368b85154a658ff6e
Mark <markhouben91@gmail.com>
2022-04-22 12:02:47 +0200

    [BUGFIX] Fix require.js frontend loading problem

--- ext_localconf.php
+++ ext_localconf.php
@@ -28,6 +28,7 @@ if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('container')) {
     ];
 }
 
-//override Localization.js
-$pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
-$pageRenderer->loadRequireJsModule('TYPO3/CMS/WvDeepltranslate/Localization');
+if (TYPO3_MODE === 'BE') {
+    $pageRenderer = \TYPO3\CMS\Core\Utility\GeneralUtility::makeInstance(\TYPO3\CMS\Core\Page\PageRenderer::class);
+    $pageRenderer->loadRequireJsModule('TYPO3/CMS/WvDeepltranslate/Localization');
+}
