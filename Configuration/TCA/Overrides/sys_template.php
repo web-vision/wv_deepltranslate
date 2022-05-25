5ee940fc4c6369842317c7662c8cb9788aa2eada
Mark <markhouben91@gmail.com>
2022-05-25 08:37:27 +0200

    [TASK] Update TCA configuration

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -1,18 +1,12 @@
 <?php
-defined('TYPO3_MODE') || die();
+if (!defined('TYPO3_MODE')) {
+    die();
+}
 
-call_user_func(function () {
-    /**
-     * Temporary variables
-     */
-    $extensionKey = 'wv_deepltranslate';
-
-    /**
-     * Default TypoScript for wv_deepltranslate
-     */
+(function () {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile(
-        $extensionKey,
+        'wv_deepltranslate',
         'Configuration/TypoScript',
         'wv_deepltranslate'
     );
-});
+})();
--- Configuration/TCA/tx_deepl_settings.php
+++ Configuration/TCA/tx_deepl_settings.php
@@ -1,41 +1,46 @@
 <?php
-/***************************************************************
-*  Copyright notice
-*
-*  (c) 2020 Ricky Mathew Kuruvilla <ricky@web-vision.de>
-*  All rights reserved
-*
-*  This script is part of the TYPO3 project. The TYPO3 project is
-*  free software; you can redistribute it and/or modify
-*  it under the terms of the GNU General Public License as published by
-*  the Free Software Foundation; either version 2 of the License, or
-*  (at your option) any later version.
-*
-*  The GNU General Public License can be found at
-*  http://www.gnu.org/copyleft/gpl.html.
-*
-*  This script is distributed in the hope that it will be useful,
-*  but WITHOUT ANY WARRANTY; without even the implied warranty of
-*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-*  GNU General Public License for more details.
-*
-*  This copyright notice MUST APPEAR in all copies of the script!
-***************************************************************/
 
-$GLOBALS['TCA']['tx_deepl_settings'] = [
+if (!defined('TYPO3_MODE')) {
+    die();
+}
+
+$ll = 'LLL:EXT:wv_deepltranslate/Resources/Private/Language/locallang_db.xlf:';
+
+return [
     'ctrl' => [
         'title' => 'Deepl settings',
         'label' => 'uid',
+        'tstamp' => 'tstamp',
+        'crdate' => 'crdate',
+        'cruser_id' => 'cruser_id',
         'iconfile' => 'EXT:wv_deepltranslate/Resources/Public/Icons/deepl.svg',
-        'hideTable' => 1,
-        'rootLevel' => 1,
+        'hideTable' => true,
+        'rootLevel' => true,
+        'sortby' => 'sorting',
+        'delete' => 'deleted',
     ],
     'columns' => [
+        'crdate' => [
+            'label' => 'crdate',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime',
+            ]
+        ],
+        'tstamp' => [
+            'label' => 'tstamp',
+            'config' => [
+                'type' => 'input',
+                'renderType' => 'inputDateTime',
+                'eval' => 'datetime',
+            ]
+        ],
         'languages_assigned' => [
             'label' => 'Deepl language assignments',
             'config' => [
                 'type' => 'input',
-                'eval' => '',
+                'eval' => 'trim',
             ],
         ],
     ],
