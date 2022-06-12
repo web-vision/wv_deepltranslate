6c2ab2b8ca4821d6ecfe327567d355bd2eaee9d7
Guido Schmechel <guido@ayacoo.de>
2022-06-12 15:48:48 +0200

    [TASK] Update supported deepl api languages
    Related #63

--- Classes/Service/DeeplService.php
+++ Classes/Service/DeeplService.php
@@ -62,7 +62,7 @@ class DeeplService
      * @see https://www.deepl.com/de/docs-api/translating-text/#request
      * @var array
      */
-    public $apiSupportedLanguages = ['BG', 'CS', 'DA', 'DE', 'EL', 'EN', 'ES', 'ET', 'FI', 'FR', 'HU', 'IT', 'JA', 'LT', 'LV', 'NL', 'PL', 'PT', 'RO', 'RU', 'SK', 'SL', 'SV', 'ZH'];
+    public $apiSupportedLanguages = ['BG', 'CS', 'DA', 'DE', 'EL', 'EN', 'ES', 'ET', 'FI', 'FR', 'HU', 'ID', 'IT', 'JA', 'LT', 'LV', 'NL', 'PL', 'PT', 'RO', 'RU', 'SK', 'SL', 'SV', 'TR', 'ZH'];
 
     /**
      * Formality supported languages
