5e84c2bf7c0d15bed23ace783f5fc5147420c05c
Mark <markhouben91@gmail.com>
2022-06-17 12:45:40 +0200

    [TASK] Add logging by localize or copy hook

--- Classes/Override/CommandMapPostProcessingHook.php
+++ Classes/Override/CommandMapPostProcessingHook.php
@@ -15,20 +15,18 @@ namespace WebVision\WvDeepltranslate\Override;
  */
 
 use B13\Container\Domain\Factory\Exception;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use TYPO3\CMS\Core\DataHandling\DataHandler;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 /**
  * Class takes care of content translation for elements within containers
  */
-class CommandMapPostProcessingHook extends \B13\Container\Hooks\Datahandler\CommandMapPostProcessingHook
+class CommandMapPostProcessingHook extends \B13\Container\Hooks\Datahandler\CommandMapPostProcessingHook implements LoggerAwareInterface
 {
-    /**
-     * @param int $uid
-     * @param int $language
-     * @param string $command
-     * @param DataHandler $dataHandler
-     */
+    use LoggerAwareTrait;
+
     protected function localizeOrCopyToLanguage(int $uid, int $language, string $command, DataHandler $dataHandler): void
     {
         try {
@@ -48,7 +46,12 @@ class CommandMapPostProcessingHook extends \B13\Container\Hooks\Datahandler\Comm
                 $localDataHandler->process_cmdmap();
             }
         } catch (Exception $e) {
-            // nothing todo
+            $this->logger->error(sprintf(
+                '%s ("%s") : %d',
+                $e->getFile(),
+                $e->getMessage(),
+                $e->getCode()
+            ));
         }
     }
 }
