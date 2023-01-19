c6ae70fe4448096d46e336a1abe7e4c803e7961f
Vishnu S Vyshakh <vishnu@web-vision.de>
2023-01-19 08:50:38 +0000

    [TASK] Init first improvement from deepl-translate flag

--- Classes/Domain/Repository/PageRepository.php
+++ Classes/Domain/Repository/PageRepository.php
@@ -3,8 +3,64 @@ declare(strict_types = 1);
 
 namespace WebVision\WvDeepltranslate\Domain\Repository;
 
+use TYPO3\CMS\Core\Database\Connection;
+use TYPO3\CMS\Core\Database\ConnectionPool;
 use TYPO3\CMS\Extbase\Persistence\Repository;
 
 final class PageRepository extends Repository
 {
+    private const TABLE_NAME = 'pages';
+
+    private ConnectionPool $connectionPool;
+
+    public function __construct()
+    {
+        $this->connectionPool = new ConnectionPool();
+    }
+
+    public function hasDeeplTranslatedContent()
+    {
+        $result = $this->connectionPool
+            ->getConnectionForTable(self::TABLE_NAME)
+            ->select(
+                ['uid', 'tx_wvdeepltranslate_has_translated_content'],
+                self::TABLE_NAME,
+                ['uid' => $this->getCurrentBackendPageId()]
+            )->fetchAssociative();
+
+        $deeplTrasalatedTime = $result['tx_wvdeepltranslate_has_translated_content'];
+        return $deeplTrasalatedTime > 0 ? true : false;
+    }
+
+    protected function getCurrentBackendPageId()
+    {
+        $pageId = null;
+        if ($GLOBALS['TYPO3_REQUEST']) {
+            $queryParams = $GLOBALS['TYPO3_REQUEST']->getQueryParams();
+            $pageId = (function () use ($queryParams) {
+                if (isset($queryParams['edit']['pages'])) {
+                    $pages = array_keys($queryParams['edit']['pages']);
+                    return $pages[0];
+                }
+            })();
+        }
+        //need more implementation
+
+        return $pageId;
+    }
+
+    public function setAsTranslatedWithDeepl($pageId)
+    {
+        $this->connectionPool
+        ->getConnectionForTable('pages')
+        ->update(
+            'pages',
+            [
+                'tx_wvdeepltranslate_has_translated_content' => 1,
+                'tx_wvdeepltranslate_translated_time' => time(),
+            ],
+            ['uid' => (int)$pageId],
+            [Connection::PARAM_INT]
+        );
+    }
 }
