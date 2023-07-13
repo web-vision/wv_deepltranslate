c5a9e3df43229ab97a0779407e84cf6dedb6bf4a
Riad <riad@web-vision.de>
2023-07-13 13:42:09 +0200

    [BUGFIX] Use proper Casing in files names

--- Configuration/JavascriptModules.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'dependencies' => ['core', 'backend'],
-    'imports' => [
-        '@typo3/backend/localization.js' => 'EXT:wv_deepltranslate/Resources/Public/JavaScript/localization.js',
-    ],
-];
