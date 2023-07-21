deea826fedd4bc5ddabfe2c390f07d130de7d970
mhofmann <m.hofmann@web-vision.de>
2023-07-21 13:50:21 +0200

    [TASK] rework to ExtensionManagementUtility::isLoaded
    As the ViewHelper should check TYPO3 extensions to be loaded, the call
    of PackageManager is not the best way.
    Call instead `ExtensionManagementUtility::isLoaded()` to check

--- Classes/ViewHelpers/Be/ExtensionActiveViewHelper.php
+++ Classes/ViewHelpers/Be/ExtensionActiveViewHelper.php
@@ -4,9 +4,7 @@ declare(strict_types=1);
 
 namespace WebVision\WvDeepltranslate\ViewHelpers\Be;
 
-use TYPO3\CMS\Core\Package\Exception\UnknownPackageException;
-use TYPO3\CMS\Core\Package\PackageManager;
-use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Core\Utility\ExtensionManagementUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
 
 /**
@@ -35,12 +33,9 @@ final class ExtensionActiveViewHelper extends AbstractConditionViewHelper
     public function render(): string
     {
         $extensionName = $this->arguments['extension'];
-        $packageManager = GeneralUtility::makeInstance(PackageManager::class);
-        try {
-            $package = $packageManager->getPackage($extensionName);
-        } catch (UnknownPackageException $_) {
-            return $this->renderElseChild();
+        if (ExtensionManagementUtility::isLoaded($extensionName)) {
+            return $this->renderThenChild();
         }
-        return $this->renderThenChild();
+        return $this->renderElseChild();
     }
 }
