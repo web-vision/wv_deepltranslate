625ef14a75711f1717d17eb6269dfa7d47bf2142
mhofmann <m.hofmann@web-vision.de>
2023-07-21 14:58:07 +0200

    [TASK] Add ViewHelper test

---
+++ Tests/Functional/ViewHelpers/ExtensionActiveViewHelperTest.php
@@ -0,0 +1,71 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebVision\WvDeepltranslate\Tests\Functional\ViewHelpers;
+
+use TYPO3\CMS\Fluid\View\TemplateView;
+use TYPO3\TestingFramework\Core\Functional\FunctionalTestCase;
+use TYPO3Fluid\Fluid\Core\Cache\FluidCacheInterface;
+use TYPO3Fluid\Fluid\Core\Cache\SimpleFileCache;
+
+final class ExtensionActiveViewHelperTest extends FunctionalTestCase
+{
+    protected bool $initializeDatabase = false;
+
+    protected array $testExtensionsToLoad = ['web-vision/wv_deepltranslate'];
+
+    protected static FluidCacheInterface $cache;
+
+    /**
+     *  Absolute path to cache directory
+     */
+    protected static string $cachePath;
+
+    public static function setUpBeforeClass(): void
+    {
+        self::$cachePath = sys_get_temp_dir() . '/' . 'fluid-functional-tests-' . sha1(__CLASS__);
+        mkdir(self::$cachePath);
+        self::$cache = (new SimpleFileCache(self::$cachePath));
+    }
+
+    public static function tearDownAfterClass(): void
+    {
+        self::$cache->flush();
+        rmdir(self::$cachePath);
+    }
+
+    public static function renderDataProvider(): \Generator
+    {
+        yield 'extension name empty, await else' => [
+            '<deepl:be.extensionActive extension="" then="thenArgument" else="elseArgument" />',
+            [],
+            'elseArgument',
+        ];
+        yield 'extension set to own, await then' => [
+            '<deepl:be.extensionActive extension="wv_deepltranslate" then="thenArgument" else="elseArgument" />',
+            [],
+            'thenArgument',
+        ];
+
+        yield 'extension set to non existent, await else' => [
+            '<deepl:be.extensionActive extension="non_existent" then="thenArgument" else="elseArgument" />',
+            [],
+            'elseArgument',
+        ];
+    }
+    /**
+     * @test
+     * @dataProvider renderDataProvider
+     * @param array<array-key, mixed> $variables
+     */
+    public function render(string $template, array $variables, string $expected): void
+    {
+        $view = new TemplateView();
+        $view->assignMultiple($variables);
+        $view->getRenderingContext()->getViewHelperResolver()->addNamespace('deepl', 'WebVision\\WvDeepltranslate\\ViewHelpers');
+        $view->getRenderingContext()->setCache(self::$cache);
+        $view->getRenderingContext()->getTemplatePaths()->setTemplateSource($template);
+        static::assertSame($expected, $view->render());
+    }
+}
