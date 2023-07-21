c975c91f18719008d343581ca406b8afe1e8b7d3
mhofmann <m.hofmann@web-vision.de>
2023-07-21 13:46:57 +0200

    [TASK] Add small documentation to ViewHelper and make class final

--- Classes/ViewHelpers/Be/ExtensionActiveViewHelper.php
+++ Classes/ViewHelpers/Be/ExtensionActiveViewHelper.php
@@ -9,20 +9,36 @@ use TYPO3\CMS\Core\Package\PackageManager;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractConditionViewHelper;
 
-class ExtensionActiveViewHelper extends AbstractConditionViewHelper
+/**
+ * condition ViewHelper for getting information about installed packages
+ * Usage:
+ * <deepl:be.extensionActive extension="enable_translated_content">
+ *     <f:then>
+ *         <!-- do stuff -->
+ *     </f:then>
+ *     <f:else>
+ *         <!-- do other stuff -->
+ *     </f:else>
+ * </deepl:be.extensionActive>
+ *
+ * Inline example:
+ * {deepl:be.extensionActive(extension: 'enable_translated_content', then: '', else: '')}
+ */
+final class ExtensionActiveViewHelper extends AbstractConditionViewHelper
 {
     public function initializeArguments(): void
     {
         parent::initializeArguments();
         $this->registerArgument('extension', 'string', 'The extension to check', true);
     }
+
     public function render(): string
     {
         $extensionName = $this->arguments['extension'];
         $packageManager = GeneralUtility::makeInstance(PackageManager::class);
         try {
             $package = $packageManager->getPackage($extensionName);
-        } catch (UnknownPackageException $e) {
+        } catch (UnknownPackageException $_) {
             return $this->renderElseChild();
         }
         return $this->renderThenChild();
