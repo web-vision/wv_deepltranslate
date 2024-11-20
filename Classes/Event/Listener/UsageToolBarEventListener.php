3376892416cb25b2adf54c65c15b90895cff8dd4
mhofmann <m.hofmann@web-vision.de>
2024-11-20 17:07:16 +0100

    [TASK] Streamline number formatting to UsageService
    For better maintenance move the severity calculation and number
    formatting to the UsageService and adjust code.

--- Classes/Event/Listener/UsageToolBarEventListener.php
+++ Classes/Event/Listener/UsageToolBarEventListener.php
@@ -7,7 +7,6 @@ namespace WebVision\WvDeepltranslate\Event\Listener;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use TYPO3\CMS\Backend\Backend\Event\SystemInformationToolbarCollectorEvent;
-use TYPO3\CMS\Backend\Toolbar\Enumeration\InformationStatus;
 use TYPO3\CMS\Core\Localization\LanguageService;
 use WebVision\WvDeepltranslate\Exception\ApiKeyNotSetException;
 use WebVision\WvDeepltranslate\Service\UsageService;
@@ -49,11 +48,15 @@ class UsageToolBarEventListener implements LoggerAwareInterface
             'LLL:EXT:wv_deepltranslate/Resources/Private/Language/locallang.xlf:usages.toolbar.message'
         );
 
-        $severity = $this->determineSeverity($usage->character->count, $usage->character->limit);
+        $severity = $this->usageService->determineSeverityForSystemInformation($usage->character->count, $usage->character->limit);
 
         $systemInformation->getToolbarItem()->addSystemInformation(
             $title,
-            sprintf($message, $this->formatNumber($usage->character->count), $this->formatNumber($usage->character->limit)),
+            sprintf(
+                $message,
+                $this->usageService->formatNumber($usage->character->count),
+                $this->usageService->formatNumber($usage->character->limit)
+            ),
             'actions-localize-deepl',
             $severity
         );
@@ -62,39 +65,5 @@ class UsageToolBarEventListener implements LoggerAwareInterface
     private function getLanguageService(): LanguageService
     {
         return $GLOBALS['LANG'];
-    }
-
-    /**
-     * Make large API limits easier to read
-     *
-     * @param int $number Any large integer - 5000000
-     * @return string Formated, better readable string variant of the integer - 5.000.000
-     */
-    private function formatNumber(int $number): string
-    {
-        return number_format($number, 0, ',', '.');
-    }
-
-
-    /**
-     * Calculate the message severity based on the quota usage rate
-     *
-     * @param int $characterCount Already translated characters in the current billing period
-     * @param int $characterLimit Total character limit in the current billing period
-     * @return string Severity level
-     */
-    private function determineSeverity(int $characterCount, int $characterLimit): string
-    {
-        $quotaUtilization = ($characterCount / $characterLimit) * 100;
-        if ($quotaUtilization >= 100) {
-            return InformationStatus::STATUS_ERROR;
-        }
-        if ($quotaUtilization >= 98) {
-            return InformationStatus::STATUS_WARNING;
-        }
-        if ($quotaUtilization >= 90) {
-            return InformationStatus::STATUS_INFO;
-        }
-        return InformationStatus::STATUS_NOTICE;
     }
 }
