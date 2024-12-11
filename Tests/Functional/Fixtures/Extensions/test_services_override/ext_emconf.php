9cdaffba4d982d3840230f73ef0e67efc9e6f4df
Stefan Bürk <stefan@buerk.tech>
2024-12-11 17:08:24 +0100

    [TASK] Ensure correct `ext_emconf.php` for `test_services_override`
    This change ensures correct `ext_emconf.php` for the
    test fixture extension `test_services_override`.

--- Tests/Functional/Fixtures/Extensions/test_services_override/ext_emconf.php
+++ Tests/Functional/Fixtures/Extensions/test_services_override/ext_emconf.php
@@ -21,7 +21,8 @@ $EM_CONF[$_EXTKEY] = [
     'version' => '1.0.0.',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.5.0-12.4.99',
+            'typo3' => '12.4.0-12.4.99',
+            'deepltranslate_core' => '*',
         ],
         'conflicts' => [],
         'suggests' => [],
