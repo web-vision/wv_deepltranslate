27a55e0dc832e8bdb8e883b0dde7bf8d8ba1d33e
Stefan Bürk <stefan@buerk.tech>
2024-12-17 10:04:00 +0100

    [TASK] Prepare UsageWidget for dual TYPO3 version compatibility
    This change moves the current `UsageWidget` implementation into a
    subfolder and modifies the `Configuration/Services.php` to ensure
    registration only for TYPO3 v12 to avoid fatal error within TYPO3
    v13.
    
    TYPO3 v13 implementation of the widget will be added in a dedicated
    change.

--- Configuration/Services.php
+++ Configuration/Services.php
@@ -5,13 +5,15 @@ namespace Symfony\Component\DependencyInjection\Loader\Configurator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 use TYPO3\CMS\Core\DependencyInjection\SingletonPass;
+use TYPO3\CMS\Core\Information\Typo3Version;
 use TYPO3\CMS\Dashboard\WidgetRegistry;
+use WebVision\Deepltranslate\Core\Core12\Widgets\UsageWidget as Core12UsageWidget;
 use WebVision\Deepltranslate\Core\Form\Item\SiteConfigSupportedLanguageItemsProcFunc;
 use WebVision\Deepltranslate\Core\Form\User\HasFormalitySupport;
 use WebVision\Deepltranslate\Core\Hooks\TranslateHook;
-use WebVision\Deepltranslate\Core\Widgets\UsageWidget;
 
 return function (ContainerConfigurator $containerConfigurator, ContainerBuilder $containerBuilder) {
+    $typo3version = new Typo3Version();
     $services = $containerConfigurator
         ->services();
 
@@ -37,19 +39,26 @@ return function (ContainerConfigurator $containerConfigurator, ContainerBuilder 
      * Registration directly in Services.yaml will break without EXT:dashboard installed!
      */
     if ($containerBuilder->hasDefinition(WidgetRegistry::class)) {
-        $services->set('widgets.deepltranslate.widget.useswidget')
-            ->class(UsageWidget::class)
-            ->arg('$view', new Reference('dashboard.views.widget'))
-            ->arg('$options', [])
-            ->tag('dashboard.widget', [
-                'identifier' => 'widgets-deepl-uses',
-                'groupNames' => 'deepl',
-                'title' => 'LLL:EXT:deepltranslate_core/Resources/Private/Language/locallang.xlf:widgets.deepltranslate.widget.useswidget.title',
-                'description' => 'LLL:EXT:deepltranslate_core/Resources/Private/Language/locallang.xlf:widgets.deepltranslate.widget.useswidget.description',
-                'iconIdentifier' => 'content-widget-list',
-                'height' => 'small',
-                'width' => 'small',
-            ])
-        ;
+        if ($typo3version->getMajorVersion() >= 13) {
+            // @todo Register TYPO3 v13 compatible UsageWidget implementation. (StandaloneView => ViewFactory)
+        }
+        /**
+         * @todo Remove core12 usage widget when TYPO3 v12 support is removed along with {@see Core11UsageWidget}.
+         */
+        if ($typo3version->getMajorVersion() === 12) {
+            $services->set('widgets.deepltranslate.widget.useswidget')
+                ->class(Core12UsageWidget::class)
+                ->arg('$view', new Reference('dashboard.views.widget'))
+                ->arg('$options', [])
+                ->tag('dashboard.widget', [
+                    'identifier' => 'widgets-deepl-uses',
+                    'groupNames' => 'deepl',
+                    'title' => 'LLL:EXT:deepltranslate_core/Resources/Private/Language/locallang.xlf:widgets.deepltranslate.widget.useswidget.title',
+                    'description' => 'LLL:EXT:deepltranslate_core/Resources/Private/Language/locallang.xlf:widgets.deepltranslate.widget.useswidget.description',
+                    'iconIdentifier' => 'content-widget-list',
+                    'height' => 'small',
+                    'width' => 'small',
+                ]);
+        }
     }
 };
